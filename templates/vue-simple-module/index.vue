ed846a6ae7f15dd7a2096160929050f2bd2186af
Jerry <jerry.hz.china@gmail.com>
2025-06-11 15:30:32 +0800

    feat: vue 简单模块模板 🔨

---
+++ templates/vue-simple-module/index.vue
@@ -0,0 +1,16 @@
+<script setup __module_lang__>
+defineOptions({
+  name: "__module_name__",
+});
+</script>
+
+<template>
+  <div class="__module_name__"></div>
+</template>
+
+<style __module_style_lang__ __module_style_scoped__>
+.__module_name__ {
+  display: flex;
+  justify-content: flex-start;
+}
+</style>
