688e80b6c2e80cd800f608e7ab65605f29f191db
Jerry <jerry.hz.china@gmail.com>
2025-06-12 13:55:07 +0800

    feat: 模板自定义配置项调整 🔨
    1. 模块顶层样式类现在由模块名称转换而来
    2. 增加了自定义数据转换规则用来对用户输入值进行可用转换

--- templates/vue-simple-module/template/index.vue
+++ templates/vue-simple-module/template/index.vue
@@ -5,11 +5,11 @@ defineOptions({
 </script>
 
 <template>
-  <div class="__module_name__"></div>
+  <div class="__module_name_class__-container"></div>
 </template>
 
 <style __module_style_lang__ __module_style_scoped__>
-.__module_name__ {
+.__module_name_class__-container {
   display: flex;
   justify-content: flex-start;
 }
